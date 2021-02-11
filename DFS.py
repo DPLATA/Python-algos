e03f18c1868ab0d1322bc6455d22db49e7e5514e
Diego G Plata <diegogplata@gmail.com>
2021-02-10 20:25:27 -0600

    ✨create DFS algorithm file🚧work in progress

---
+++ DFS.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+
+
+if __name__ == '__main__':
+    print('a')
